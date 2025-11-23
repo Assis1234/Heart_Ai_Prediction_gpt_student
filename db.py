dd3c1a5b78560b20ecd90f2b50a184d8037cbac9
Assis1234 <targetblockadds@gmail.com>
2025-11-23 13:14:11 +0530

    Update db.py

--- db.py
+++ db.py
@@ -4,9 +4,9 @@ import os
 
 def get_connection():
     conn = mysql.connector.connect(
-        host=os.getenv("DB_HOST", "localhost"),       # default = localhost
+        host=os.getenv("DB_HOST", "trolley.proxy.rlwy.net"),       # default = localhost
         user=os.getenv("DB_USER", "root"),            # default = root
-        password=os.getenv("DB_PASSWORD", "NewPassword123"),  # default = your local pwd
+        password=os.getenv("DB_PASSWORD", "uvAuucWtdFXOyEaihxcguLBnslOLMNTi"),  # default = your local pwd
         database=os.getenv("DB_NAME", "heart_patient"),       # default = your local DB
         port=int(os.getenv("DB_PORT", 18222))          # default = 3306
     )
