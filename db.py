2dd01ba38b92c151ffb1bdba075b390c48084964
Assis1234 <targetblockadds@gmail.com>
2025-11-23 12:24:49 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,11 +1,13 @@
 # db.py
 import mysql.connector
+import os
 
 def get_connection():
     conn = mysql.connector.connect(
-    host="localhost",       # your MySQL host
-    user="root",            # your MySQL username
-    password="NewPassword123",            # your MySQL password
-    database="heart_patient"
+        host=os.getenv("DB_HOST", "localhost"),       # default = localhost
+        user=os.getenv("DB_USER", "root"),            # default = root
+        password=os.getenv("DB_PASSWORD", "NewPassword123"),  # default = your local pwd
+        database=os.getenv("DB_NAME", "heart_patient"),       # default = your local DB
+        port=int(os.getenv("DB_PORT", 3306))          # default = 3306
     )
     return conn
